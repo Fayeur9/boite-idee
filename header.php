ea94f338151170401a0c1ddd9b12c9aec078a0d5
Lenny Moinardeau <moinardeaulenny2004@gmail.com>
2024-11-16 22:52:25 +0100

    Modif graphique menu

--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <header>
     <nav>
-        <a href="mes_idees.php" class="btn btn-secondary p-4">Mes Idées</a>
-        <a href="liste_idees.php" class="btn btn-secondary p-4">Toutes les Idées</a>
-        <a href="creation_idees.php" class="btn btn-secondary p-4">Nouvelle Idée</a>
-        <a href="deconnexion.php" class="btn btn-secondary p-4">Déconnexion</a>
+        <a href="mes_idees.php" class="btn btn-secondary">Mes Idées</a>
+        <a href="liste_idees.php" class="btn btn-secondary">Toutes les Idées</a>
+        <a href="creation_idees.php" class="btn btn-secondary">Nouvelle Idée</a>
+        <a href="deconnexion.php" class="btn btn-secondary">Déconnexion</a>
     </nav>
 </header>
